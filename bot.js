6a88ae9f06f35bbc91eae6d68a4e8f245cbd5290
emilia4315 <37253376+emilia4315@users.noreply.github.com>
2018-03-11 12:11:42 +0000

    small changes
    nothing big

--- bot.js
+++ bot.js
@@ -21,4 +21,15 @@ client.on('message', message => {
 	}
 });
 
+// ping command using latency
+
+client.on('message', message => {
+	if (message.content === Prefix + 'ping') {
+	const m = await message.channel.send("`Testing ping...`");
+    m.edit(`Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+	}
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
